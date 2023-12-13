09f674fd02e5906af4241bdb5ff84883021c66cb
@jiangwan <18337600696@163.com>
2023-12-13 10:08:05 +0800

    feat:新增4.0

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -530,10 +530,8 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
-            <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
-            </div>
+            <div className={styles["main-title"]}>AI ChatGPT Next Web</div>
+            <div className={styles["sub-title"]}>WX:LIANJIAyuhaoyue</div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
               <span className={styles["icon-space"]}>&</span>
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,11 +155,9 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          AI ChatGPT Next Web
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
+        <div className={styles["sidebar-sub-title"]}>WX:LIANJIAyuhaoyue</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
